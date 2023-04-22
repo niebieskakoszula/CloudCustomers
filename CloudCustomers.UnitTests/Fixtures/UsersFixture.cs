25d3be424c3e381d5d6feed3358e099a89c7bf54
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-04-22 16:39:34 +0200

    helpers

---
+++ CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
@@ -0,0 +1,50 @@
+﻿using CloudCustomers.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+	public static class UsersFixture
+	{
+		public static List<User> GetTestUsers() =>
+			new()
+			{
+				new User
+				{
+					Name = "Tester1",
+					Email = "user.tester1@dev.com",
+					Address = new Address
+					{
+						Street ="al. Jana Pawła II 21/37",
+						City = "Inowrocław",
+						ZipCode = "09-123"
+					}
+				},
+				new User
+				{
+					Name = "Tester2",
+					Email = "user.tester2@dev.com",
+					Address = new Address
+					{
+						Street ="al. Jana Pawła II 21/37",
+						City = "Inowrocław",
+						ZipCode = "09-123"
+					}
+				},
+				new User
+				{
+					Name = "Tester3",
+					Email = "user.tester3@dev.com",
+					Address = new Address
+					{
+						Street ="al. Jana Pawła II 21/37",
+						City = "Inowrocław",
+						ZipCode = "09-123"
+					}
+				}
+			};
+	}
+}
